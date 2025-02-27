d7ab888ebfdb3cd4b30f355347dd70b804fc3c1a
ousttrue <ousttrue@gmail.com>
2025-02-28 00:44:21 +0900

    triangle

---
+++ src/01_triangle.md
@@ -0,0 +1,17 @@
+# bun + view + typescript で WGPU を練習
+
+初期化。
+
+https://ja.vite.dev/guide/
+
+```sh
+$ mkdir hello_wgsl
+$ cd hello_wgsl
+$ bun create vite .
+$ bun install
+$ bun run dev
+```
+
+三角形描画。
+
+- 参考 [WebGPU と TypeScript で始める、次世代のグラフィックス開発入門](https://zenn.dev/blueteam/articles/576b7f1f5fd034)
---
+++ src/main.ts
@@ -0,0 +1,84 @@
+// shader の import には ?raw が必要
+import vertexShader from './vertex.wgsl?raw'
+import fragmentShader from './fragment.wgsl?raw'
+
+function run(device: GPUDevice, canvas: HTMLCanvasElement, context: GPUCanvasContext) {
+  const { devicePixelRatio } = window
+  canvas.width = devicePixelRatio * canvas.clientWidth
+  canvas.height = devicePixelRatio * canvas.clientHeight
+
+  const presentationFormat = navigator.gpu.getPreferredCanvasFormat()
+
+  context.configure({
+    device,
+    format: presentationFormat,
+    alphaMode: 'premultiplied',
+  })
+
+  const pipeline = device.createRenderPipeline({
+    layout: 'auto',
+    vertex: {
+      module: device.createShaderModule({
+        code: vertexShader,
+      }),
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: fragmentShader,
+      }),
+      targets: [
+        {
+          format: presentationFormat,
+        },
+      ],
+    },
+    primitive: {
+      topology: 'triangle-list',
+    },
+  })
+
+  function frame() {
+    const textureView = context.getCurrentTexture().createView()
+    const renderPassDescriptor: GPURenderPassDescriptor = {
+      colorAttachments: [
+        {
+          view: textureView,
+          clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+          loadOp: 'clear',
+          storeOp: 'store',
+        },
+      ],
+    }
+
+    const commandEncoder = device.createCommandEncoder()
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor)
+    {
+      passEncoder.setPipeline(pipeline)
+      passEncoder.draw(3)
+    }
+    passEncoder.end()
+    device.queue.submit([commandEncoder.finish()])
+
+    requestAnimationFrame(frame)
+  }
+  requestAnimationFrame(frame)
+}
+
+async function initAsync() {
+  const canvas: HTMLCanvasElement = document.querySelector('canvas')!
+
+  const adapter = await navigator.gpu.requestAdapter()
+  if (!adapter) {
+    throw new Error('no adapter')
+  }
+
+  const device = await adapter.requestDevice()
+  const context = canvas.getContext('webgpu')
+  if (!context) {
+    throw new Error('no context')
+  }
+
+  run(device, canvas, context);
+}
+
+initAsync()
