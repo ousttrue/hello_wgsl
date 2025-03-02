6d43f9291e1b7d559009b059f5f7e7427434870a
ousttrue <ousttrue@gmail.com>
2025-03-02 23:09:38 +0900

    mdx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,11 +3,11 @@ import posts from '../posts';
 export default function Home() {
   return (<>
     <ul>
-      <li><a href={`${import.meta.env.BASE_URL}01_triangle/`}>01_triangle</a></li>
+      <li><a href={`${import.meta.env.BASE_URL}01_canvas/`}>01_triangle</a></li>
       <li><a href={`${import.meta.env.BASE_URL}02_wgpu/`}>02_wgpu</a></li>
       {posts.map((post, i) => (<li key={i}>
         <a href={post.path}>
-          post: {post.data.frontmatter.title}
+          {post.data.frontmatter.title}
         </a>
       </li>))}
     </ul>
